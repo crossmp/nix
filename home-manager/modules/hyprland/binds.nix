41062f658defcc167eeb8ba1caa8ba147f8ce44a
matt <mpcross19@gmail.com>
2025-08-06 20:48:29 +1200

    update

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -100,12 +100,12 @@
       # Extras
       # VolUp VolDown MicMute Controll
 
-      ", XF86AudioRaiseVolume,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-      ", XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-      ", XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ", XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ", XF86MonBrightnessUp,   exec, brightnessctl -e4 -n2 set 5%+"
-      ", XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      #", XF86AudioRaiseVolume,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+      #", XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      #", XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      #", XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      #", XF86MonBrightnessUp,   exec, brightnessctl -e4 -n2 set 5%+"
+      #", XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
       
       #", XF86Sleep, exec, loginctl lock-session"
       #", XF86Launch3, exec, asusctl -c 60"
